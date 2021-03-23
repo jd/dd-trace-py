0709dd7a00056d0d9dee774ccf08ea6488c577fe
Julien Danjou <julien@danjou.info>
2021-03-23 17:08:24 +0100

    refactor(service): add ServiceProcess class
    This adds a new class ServiceProcess which is able to handle the lifecycle of
    Service with respect to its processes (fork, exit, etc).

--- ddtrace/internal/service.py
+++ ddtrace/internal/service.py
@@ -1,7 +1,19 @@
+# -*- encoding: utf-8 -*-
+import atexit
 import enum
+import os
 import threading
+import typing
 
+from ddtrace.internal import forksafe
+from ddtrace.internal import uwsgi
 from ddtrace.vendor import attr
+
+
+from .logger import get_logger
+
+
+log = get_logger(__name__)
 
 
 class ServiceStatus(enum.Enum):
@@ -20,9 +32,10 @@ class Service(object):
     """A service that can be started or stopped."""
 
     status = attr.ib(default=ServiceStatus.STOPPED, type=ServiceStatus, init=False, eq=False)
-    _service_lock = attr.ib(factory=threading.Lock, repr=False, init=False, eq=False)
+    _service_lock = attr.ib(factory=threading.Lock, repr=False, init=False, eq=False, type=threading.Lock)
 
     def __enter__(self):
+        # type: () -> Service
         self.start()
         return self
 
@@ -31,6 +44,7 @@ class Service(object):
         self.join()
 
     def start(self):
+        # type: () -> None
         """Start the service."""
         # Use a lock so we're sure that if 2 threads try to start the service at the same time, one of them will raise
         # an error.
@@ -48,9 +62,94 @@ class Service(object):
         """
 
     def stop(self):
+        # type: () -> None
         """Stop the service."""
         self.status = ServiceStatus.STOPPED
 
     @staticmethod
-    def join(timeout=None):
+    def join(
+        timeout=None,  # type: float
+    ):
+        # type: (...) -> typing.Optional[float]
         """Join the service once stopped."""
+
+    def copy(self):
+        # type: () -> Service
+        return attr.evolve(self)
+
+
+class ServiceProcess(object):
+    """A manager that can handle the lifecycle of a service in a process.
+
+    This allows to handle a service that should keep running when, e.g., the Python process forks."""
+
+    SERVICE_CLASS = None  # type: typing.ClassVar[typing.Type[Service]]
+
+    def __init__(self, *args, **kwargs):
+        # type: (...) -> None
+        super(ServiceProcess, self).__init__()
+        self._service = self.SERVICE_CLASS(*args, **kwargs)
+
+    def start(self, *args, **kwargs):
+        # type: (...) -> None
+        """Start the service.
+
+        :param stop_on_exit: Whether to stop the service and wait for shutdown on exit.
+        :param start_in_children: Whether to start the service in child processes.
+        """
+        stop_on_exit = kwargs.pop("stop_on_exit", True)
+        start_in_children = kwargs.pop("start_in_children", True)
+
+        if start_in_children:
+            try:
+                uwsgi.check_uwsgi(self.start, atexit=self.stop if stop_on_exit else None)
+            except uwsgi.uWSGIMasterProcess:
+                # Do nothing, the start() method will be called in each worker subprocess
+                return
+
+        self._service.start()
+
+        if stop_on_exit:
+            atexit.register(self.stop)
+
+        if start_in_children:
+            forksafe.register(self._restart_on_fork)
+            # if hasattr(os, "register_at_fork"):
+            #     os.register_at_fork(after_in_child=self._restart_on_fork)
+            # else:
+            #     log.warning(
+            #         "Your Python version does not have `os.register_at_fork`. "
+            #         "You have to start a new instance of %s after fork() manually." % self.__class__.__name__
+            #     )
+
+    def stop(self, *args, **kwargs):
+        # type: (...) -> None
+        """Stop the service."""
+        # Check for the service status: this *should* not be necessary per-say, but in the case `atexit.unregister` is
+        # unavailable, we're sure not to call stop twice.
+        if self._service.status == ServiceStatus.RUNNING:
+            self._service.stop(*args, **kwargs)
+
+        # Python 2 does not have unregister
+        if hasattr(atexit, "unregister"):
+            # You can unregister a method that was not registered, so no need to do any other check
+            atexit.unregister(self.stop)
+
+        try:
+            forksafe.unregister(self._restart_on_fork)
+        except ValueError:
+            pass
+
+    def __getattr__(self,
+                    k, # type: typing.Any
+                    ):
+        # type: (...) -> typing.Any
+        return getattr(self._service, k)
+
+    def _restart_on_fork(self):
+        # type: () -> None
+        # Be sure to stop the parent first, since it might have to e.g. unpatch functions
+        # Do not flush data as we don't want to have multiple copies of the parent profile exported.
+        self.stop()
+        self._service = self._service.copy()
+        self.start()
--- tests/tracer/test_service.py
+++ tests/tracer/test_service.py
@@ -1,3 +1,7 @@
+import subprocess
+import sys
+
+from ddtrace import compat
 from ddtrace.internal import service
 
 
@@ -12,3 +16,177 @@ def test_service_status():
     assert s.status == service.ServiceStatus.RUNNING
     s.stop()
     assert s.status == service.ServiceStatus.STOPPED
+
+
+def test_service_copy():
+    s = service.Service()
+    assert s.copy() == s.copy()
+    assert s.copy() is not s.copy()
+    s.start()
+    s2 = s.copy()
+    assert s is not s2
+    assert s == s2
+    assert s.status == service.ServiceStatus.RUNNING
+    assert s2.status == service.ServiceStatus.STOPPED
+    s.stop()
+    assert s is not s2
+    assert s == s2
+    assert s.status == service.ServiceStatus.STOPPED
+    assert s2.status == service.ServiceStatus.STOPPED
+
+
+def test_service_manager():
+    class MyService(service.Service):
+        pass
+
+    class MyServiceProcess(service.ServiceProcess):
+        SERVICE_CLASS = MyService
+
+    sm = MyServiceProcess()
+    sm.start()
+    assert sm.status == sm._service.status == service.ServiceStatus.RUNNING
+
+    sm.stop()
+    assert sm.status == sm._service.status == service.ServiceStatus.STOPPED
+
+
+def call_program(*args):
+    subp = subprocess.Popen(
+        args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        close_fds=True,
+    )
+    stdout, stderr = subp.communicate()
+    return stdout, stderr, subp.wait(), subp.pid
+
+
+def test_service_manager_stop_on_exit(tmpdir):
+    pyfile = tmpdir.join("test.py")
+    pyfile.write(
+        """
+from ddtrace.internal import service
+
+class MyService(service.Service):
+    def stop(self):
+        print("I was stopped, thank you")
+
+class MyServiceProcess(service.ServiceProcess):
+    SERVICE_CLASS = MyService
+
+sm = MyServiceProcess()
+sm.start()
+""")
+    out, err, status, pid = call_program(sys.executable, str(pyfile))
+    assert status == 0
+    assert out == b"I was stopped, thank you\n"
+    assert err == b""
+
+
+def test_service_manager_stop_on_exit_false(tmpdir):
+    pyfile = tmpdir.join("test.py")
+    pyfile.write(
+        """
+from ddtrace.internal import service
+
+class MyService(service.Service):
+    def stop(self):
+        print("I was stopped, thank you")
+
+class MyServiceProcess(service.ServiceProcess):
+    SERVICE_CLASS = MyService
+
+sm = MyServiceProcess()
+sm.start(stop_on_exit=False)
+""")
+    out, err, status, pid = call_program(sys.executable, str(pyfile))
+    assert status == 0
+    assert out == b""
+    assert err == b""
+
+
+def test_service_manager_start_in_children_false(tmpdir):
+    pyfile = tmpdir.join("test.py")
+    pyfile.write(
+        """
+import os
+import sys
+
+from ddtrace.internal import service
+
+class MyService(service.Service):
+    def stop(self):
+        super(MyService, self).stop()
+        print("I was stopped, thank you")
+
+class MyServiceProcess(service.ServiceProcess):
+    SERVICE_CLASS = MyService
+
+sm = MyServiceProcess()
+sm.start(start_in_children=False)
+
+parent_service_instance = sm._service
+
+child_pid = os.fork()
+if child_pid == 0:
+    assert parent_service_instance.status == service.ServiceStatus.RUNNING
+    assert sm._service == parent_service_instance
+    assert sm._service is parent_service_instance
+    assert sm.status == service.ServiceStatus.RUNNING
+else:
+    assert sm._service is parent_service_instance
+    assert sm.status == service.ServiceStatus.RUNNING
+    pid, status = os.waitpid(child_pid, 0)
+    sys.exit(os.WEXITSTATUS(status))
+""")
+    out, err, status, pid = call_program(sys.executable, str(pyfile))
+    assert status == 0
+    # Stopped 2 times:
+    # parent stopped in child (at exit())
+    # parent stopped in parent (at exit())
+    assert out == b"I was stopped, thank you\nI was stopped, thank you\n"
+    assert err == b""
+
+
+def test_service_manager_start_in_children(tmpdir):
+    pyfile = tmpdir.join("test.py")
+    pyfile.write(
+        """
+import os
+import sys
+
+from ddtrace.internal import service
+
+class MyService(service.Service):
+    def stop(self):
+        super(MyService, self).stop()
+        print("I was stopped, thank you")
+
+class MyServiceProcess(service.ServiceProcess):
+    SERVICE_CLASS = MyService
+
+sm = MyServiceProcess()
+sm.start()
+
+parent_service_instance = sm._service
+
+child_pid = os.fork()
+if child_pid == 0:
+    assert parent_service_instance.status == service.ServiceStatus.STOPPED
+    assert sm._service == parent_service_instance
+    assert sm._service is not parent_service_instance
+    assert sm.status == service.ServiceStatus.RUNNING
+else:
+    assert sm._service is parent_service_instance
+    assert sm.status == service.ServiceStatus.RUNNING
+    pid, status = os.waitpid(child_pid, 0)
+    sys.exit(os.WEXITSTATUS(status))
+""")
+    out, err, status, pid = call_program(sys.executable, str(pyfile))
+    assert status == 0
+    # Stopped 3 times:
+    # parent stopped in the child (at fork())
+    # child stopped in child (at exit())
+    # parent stopped in parent (at exit())
+    assert out == b"I was stopped, thank you\nI was stopped, thank you\nI was stopped, thank you\n"
+    assert err == b""
